13072d7d27495a12a6b92fa2038ece7034b4db72
JoelDamian <joel.damian.echeverria@gmail.com>
2025-06-01 11:51:01 -0400

    Added functionality to live a cell.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { useState} from 'react';
 import './App.css';
 
 const NUM_ROWS = 30;
 const NUM_COLS = 30;
 
-const grid: boolean[][] = Array.from({ length: NUM_ROWS }, () =>
-  Array(NUM_COLS).fill(false)
-);
+type Grid = boolean[][];
+
+const createEmptyGrid = (): Grid =>
+  Array.from({ length: NUM_ROWS }, () => Array(NUM_COLS).fill(false));
 
 function App() {
+   const [grid, setGrid] = useState<Grid>(createEmptyGrid);
+
+  const handleToggleCell = (i: number, j: number) => {
+    const newGrid = grid.map((row, rowIndex) =>
+      row.map((cell, colIndex) =>
+        rowIndex === i && colIndex === j ? !cell : cell
+      )
+    );
+    setGrid(newGrid);
+  };
+
+  const resetGrid = () => {
+    setGrid(createEmptyGrid());
+  };
+
   return (
     <div className='app-container'>
       <h1>Conway's Game of Life</h1>
@@ -21,6 +38,7 @@ function App() {
           row.map((cell: boolean, j: number) => (
             <div
               key={`${i}-${j}`}
+              onClick={() => handleToggleCell(i, j)}
               style={{
                 width: 20,
                 height: 20,
@@ -31,6 +49,9 @@ function App() {
           ))
         )}
       </div>
+      <button onClick={resetGrid}>
+        Reset
+      </button>
     </div>
   );
 }
